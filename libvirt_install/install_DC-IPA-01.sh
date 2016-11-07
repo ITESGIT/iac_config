61a96fbcebb886463c4d704ccf0f3e1ce48d98be
Luke Pafford <lukepafford@gmail.com>
2016-11-06 17:30:03 -0800

    Removed domain name for DC-IPA-01 virtual machine name in libvirt install script

--- libvirt_install/install_DC-IPA-01.sh
+++ libvirt_install/install_DC-IPA-01.sh
@@ -2,8 +2,8 @@
 
 virt-install \
 --connect qemu+ssh://lukepafford@VH-KVM-01/system \
---name DC-IPA-01.lukepafford.com \
---disk path=/var/lib/libvirt/images/DC-IPA-01.lukepafford.com.img,size=35 \
+--name DC-IPA-01 \
+--disk path=/var/lib/libvirt/images/DC-IPA-01,size=35 \
 --memory 2048 \
 --vcpus 2 \
 -l http://192.168.1.64/inst \
