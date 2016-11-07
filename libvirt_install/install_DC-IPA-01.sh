63667f0944176c9acc1220ff553e6f4a1e0b3b71
Luke Pafford <lukepafford@gmail.com>
2016-11-07 00:03:06 -0800

    Modified DC-IPA-01 virt-install file to use vnc graphics, and autostart

--- libvirt_install/install_DC-IPA-01.sh
+++ libvirt_install/install_DC-IPA-01.sh
@@ -6,6 +6,8 @@ virt-install \
 --disk path=/var/lib/libvirt/images/DC-IPA-01,size=35 \
 --memory 2048 \
 --vcpus 2 \
+--graphics vnc \
+--autostart \
 -l http://192.168.1.64/inst \
 -x ks=http://192.168.1.64/files/freeipa_ks.cfg \
 
