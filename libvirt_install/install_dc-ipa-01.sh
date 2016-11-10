cfbd1d502417127bd2fa1e308aa4c72e8ec637ee
Luke Pafford <lukepafford@gmail.com>
2016-11-09 21:15:00 -0800

    Implemented most of neccessary changes to automate install of Freeipa Server. Post install section works manually, but fails in script for unknown reason

--- libvirt_install/install_dc-ipa-01.sh
+++ libvirt_install/install_dc-ipa-01.sh
@@ -2,12 +2,13 @@
 
 virt-install \
 --connect qemu+ssh://lukepafford@VH-KVM-01/system \
---name DC-IPA-01 \
+--name dc-ipa-01 \
 --disk path=/var/lib/libvirt/images/dc-ipa-01,size=35 \
---memory 2048 \
---vcpus 2 \
+--memory 1024 \
+--vcpus 1 \
+--network bridge=br0  \
 --graphics vnc \
 --autostart \
 -l http://192.168.1.64/inst \
--x ks=http://192.168.1.64/files/freeipa_ks.cfg \
+-x ks=http://192.168.1.64/files/dc-ipa-01_ks.cfg \
 
