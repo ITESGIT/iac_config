14625cef26eb976116e3ef25547794868e3a641d
Luke Pafford <lukepafford@gmail.com>
2016-11-12 01:33:42 -0800

    Added password prompt in dc-ipa-01 post install script
    After looking through the ipa-server-install man page, and google, I found no way to pass an encrypted password to the command. The next best solution was to prompt for the password in the script, but i was hoping I could make it as automated as possible.

--- post_install/dc-ipa-01_post_install.sh
+++ post_install/dc-ipa-01_post_install.sh
@@ -1,3 +1,8 @@
+# Prompt for password to be used to configure all accounts. This single password is just used
+# For lab purposes. Prod should have one password for each account
+
+read -s -p "Enter password to be used for freeipa directory server, and admin accounts: "
+
 /usr/bin/echo "192.168.1.10 dc-ipa-01.lukepafford.com dc-ipa-01" >> /etc/hosts
 /usr/bin/echo "dc-ipa-01.lukepafford.com" > /etc/hostname
 
@@ -8,7 +13,7 @@ echo GAETWAY=192.168.1.254 >> /etc/sysconfig/network
 # Create lukepafford admin account
 useradd lukepafford
 usermod -aG wheel lukepafford
-echo *encrypted password* | passwd --stdin lukepafford
+echo "$REPLY" | passwd --stdin lukepafford
 
 yum install ipa-server -y
 yum install ipa-server-dns -y
@@ -18,5 +23,5 @@ yum install ipa-server-dns -y
 yum install haveged -y
 systemctl start haveged
 
-ipa-server-install --unattended --realm=LUKEPAFFORD.com --domain=lukepafford.com --ds-password=*encrypted password* --admin-password=*encrypted password* --hostname=dc-ipa-01.lukepafford.com --ip-address=192.168.1.10 --setup-dns --forwarder=8.8.8.8 --forwarder=8.8.4.4
+ipa-server-install --unattended --realm=LUKEPAFFORD.com --domain=lukepafford.com --ds-password="$REPLY" --admin-password="$REPLY" --hostname=dc-ipa-01.lukepafford.com --ip-address=192.168.1.10 --setup-dns --forwarder=8.8.8.8 --forwarder=8.8.4.4
 
