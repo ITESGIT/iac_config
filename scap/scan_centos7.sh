079fa6ddeb9f2fcfe57356690b5d50ecd4d5c9af
Luke Pafford <lukepafford@gmail.com>
2016-11-14 00:10:49 -0800

    Modified date function to output proper formatting

--- scap/scan_centos7.sh
+++ scap/scan_centos7.sh
@@ -3,7 +3,7 @@
 # installed
 
 HOSTNAME="$(hostname)"
-DATE="$(date +%Y-%m-%d:%H:%M:%S)" 
+DATE="$(date +%Y-%m-%d_%H:%M:%S)" 
 
 oscap xccdf eval --profile stig-rhel7-server-upstream --results-arf "$HOSTNAME"_"$DATE"-xccdf_results.xml --report "$HOSTNAME"_"$DATE"-xccdf_results.html /usr/share/xml/scap/ssg/content/ssg-centos7-xccdf.xml
 
