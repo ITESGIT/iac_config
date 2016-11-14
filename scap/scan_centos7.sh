e9aab0441f236cd9c716aba40bff835a1194a9d4
Luke Pafford <lukepafford@gmail.com>
2016-11-13 23:48:40 -0800

    Created directory for openscap scan scripts, and results to show tool capabilities

---
+++ scap/scan_centos7.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# To run this command you must have the open-scap package
+# installed
+
+HOSTNAME="$(hostname)"
+DATE="$(date +%Y-%m-%d:%H:%M:%S)" 
+
+oscap xccdf eval --profile stig-rhel7-server-upstream --results-arf "$HOSTNAME"_"$DATE"-xccdf_results.xml --report "$HOSTNAME"_"$DATE"-xccdf_results.html /usr/share/xml/scap/ssg/content/ssg-centos7-xccdf.xml
+
