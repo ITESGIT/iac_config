b06fc58b2adc7f99a6d9c53bf62871fdf25215cb
Luke Pafford <lukepafford@gmail.com>
2017-01-27 23:15:52 -0800

    Modified DNS configurations to reflect new subnet

--- bash/update_master_dns.sh
+++ bash/update_master_dns.sh
@@ -11,8 +11,8 @@ sudo /usr/bin/chmod 755 /var/named
 sudo /usr/bin/chown server_config:service_accounts /home/server_config
 
 # Copy master named conf and zone files from config server to dns server.
-scp server_config@centos7-l01:/home/lukepafford/Documents/iac_config/configuration_files/dns/master_named.conf ~
-scp server_config@centos7-l01:/home/lukepafford/Documents/iac_config/configuration_files/dns/lukepafford.com* ~
+#scp server_config@centos7-l01:/home/lukepafford/Documents/iac_config/configuration_files/dns/master_named.conf ~
+#scp server_config@centos7-l01:/home/lukepafford/Documents/iac_config/configuration_files/dns/lukepafford.com* ~
 
 # Replace named conf file and set proper permissions
 sudo /usr/bin/mv /home/server_config/master_named.conf /etc/named.conf
