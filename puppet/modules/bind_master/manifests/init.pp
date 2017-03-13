e535a164330169120399151714d4b30d0fb4bb43
Luke Pafford <lukepafford@gmail.com>
2017-03-13 01:29:48 -0700

    Modified bind_master manifest file to add class parameters

--- puppet/modules/bind_master/manifests/init.pp
+++ puppet/modules/bind_master/manifests/init.pp
@@ -1,42 +1,42 @@
-class bind_master ($domain) { 
-  
+class bind_master (
+$domain,
+$mode = '0640',
+$owner = 'root',
+$group = 'named',
+$confdir = '/etc',
+$zonedir = '/var/named',
+) 
+{ 
     case $::operatingsystem {
         'centos': { $bind_pkg = 'bind' }
-        default:  { fail("unrecognized operating system") }
+         default:  { fail("unrecognized operating system") }
     }
-
     package { $bind_pkg:
         ensure => present,
     }
-
-    file { '/etc/named.conf':
+    file { '${confdir}/named.conf':
         ensure => file,
         mode => '0640',
-        owner => 'root',
-        group => 'named',
+        owner => '${owner}',
+        group => '${group}',
         source => 'puppet:///modules/bind_master/named.conf',
       	require => Package["$bind_pkg"],
     }
-
-    file { "/var/named/${domain}.zone":
+    file { "${zonedir}/${domain}.zone":
         ensure => file,
         mode => '0640',
-        owner => 'root',
-        group => 'named',
         source => "puppet:///modules/bind_master/${domain}.zone",
       	require => Package["$bind_pkg"],
     }
-
-     file { "/var/named/${domain}.rr.zone":
+    file { "${zonedir}/${domain}.rr.zone":
         ensure => file,
         mode => '0640',
-        owner => 'root',
-        group => 'named',
+        owner => '${owner}',
+        group => '${group}',
         source => "puppet:///modules/bind_master/${domain}.rr.zone",
       	require => Package["$bind_pkg"],
     }
-
-   service { 'named':
+   service { '${group}':
       ensure => running,
       enable => true,
       require => Package["$bind_pkg"],
